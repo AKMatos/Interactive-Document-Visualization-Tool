1ed29fd29e7101709956a91819e489338e386e5c
jmatos6 <jmatos6@umassd.edu>
2026-04-26 23:25:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -302,3 +302,114 @@ function getDragAfterWorkspace(container, x) {
     }
     }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 900 - margin.left - margin.right,
+    height = 900 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+	.attr("style", "background-color: white")
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+
+//Use d3 to read the csv and do the then section on read
+d3.csv("MDS.csv",d=>{
+    return{
+        fileName: String(d.FileNames),
+        x: String(d.x),
+        y: String(d.y),
+    }
+}).then(data=>{
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([-2400, 100])
+        .range([ 0, width ]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([-100, 2400])
+        .range([ height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Add a tooltip div. Here I define the general feature of the tooltip: stuff that do not depend on the data point.
+    // Its opacity is set to 0: we don't see it by default.
+    var tooltip = d3.select("#my_dataviz")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+
+
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    var mouseover = function(d) {
+        tooltip
+        .style("opacity", 1)
+    }
+
+    var mousemove = function(d) {
+        tooltip
+        .html("File " + d.fileNames + ": " + d.x + ", " + d.y)
+        // .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+        // .style("top", (d3.mouse(this)[1]) + "px")
+    }
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    var mouseleave = function(d) {
+        tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+    }
+
+    // Add dots
+    svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { 
+            console.log("here")
+            return x(d.x); } )
+        .attr("cy", function (d) { return y(d.y); } )
+        .attr("r", 7)
+        .style("fill", function (d) {
+            // If file name contains the "d" it is a docs file, so color it different
+            let colorBlue = false
+            for (let i = 0; i <= d.fileName.length; i++) {
+                if (d.fileName[i] == 'd') {
+                    colorBlue = true
+                }
+            }
+            if (colorBlue) {
+                return "#69b3a2"
+            }
+            else {
+                return "#966b6b"
+            }
+        })
+        .style("opacity", 0.65)
+        .style("stroke", "white")
+        .on("mouseover", mouseover )
+        .on("mousemove", mousemove )
+        .on("mouseleave", mouseleave )
+});
